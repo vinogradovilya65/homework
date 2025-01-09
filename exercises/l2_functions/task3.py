51533a19eb6071575fd1d9125ac8d2d579fa5b9d
mikhailkravets <michkravets@gmail.com>
2025-01-09 21:12:44 +0200

    Add tasks on functions

---
+++ exercises/l2_functions/task3.py
@@ -0,0 +1,29 @@
+from typing import Iterable
+
+
+# Finish the function to make the script to work without errors.
+#
+# NOTE:
+# Iterable is any object that can be converted into an iterator.
+# You're already familiar with `list`, `str` which are Iterables
+def reverse(c: Iterable) -> Iterable:
+    pass
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert reverse([1, 2, 3]) == [3, 2, 1]
+    assert reverse("abc") == "cba"
+
+    # NOTE:
+    # Here we used a new type to you called tuple.
+    # Tuple is an immutable collection of values defined inside parentheses,
+    # as you see below. Often, we can omit the parentheses and write something
+    # like this
+    #
+    # x = 3, 5, 6
+    #
+    # which is equivalent to
+    #
+    # x = (3, 5, 6)
+    assert reverse((3, 4, 5)) == (5, 4, 3)

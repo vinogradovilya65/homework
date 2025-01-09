fc153a8c541afc3429340c9ac3d14d91167717da
mikekravets <michkravets@gmail.com>
2025-01-09 13:08:41 +0200

    Add more tasks to variable and math lecture

---
+++ exercises/l1_variables_and_math/task11.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    # Do not change the line below
+    s = "Hello World"
+
+    # Modify variable c using string s to make this script to work without errors
+    c = None
+
+    # Do not change the line below
+    assert c == "HloWrd"
---
+++ exercises/l1_variables_and_math/task7.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    # Do not change the line below
+    l = [1, 2, 5, 0, -3]
+
+    # Modify variable c using list l to make this script to work without errors
+    c = None
+
+    # Do not change the line below
+    assert c == 5

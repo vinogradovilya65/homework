182756c862ed3d3dd0183eed74e04eb35a6a6ea3
mikhailkravets <michkravets@gmail.com>
2025-01-13 16:36:56 +0200

    Add 7 task on loops

---
+++ exercises/l4_loops/task7.py
@@ -0,0 +1,12 @@
+# Use `while` loop to calculate the number
+# of digits in a number `n`
+def count_digits(n: int) -> int:
+    pass
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert count_digits(0) == 1
+    assert count_digits(134) == 3
+    assert count_digits(54) == 2
+    assert count_digits(55678) == 5

722e4196d1cae8a44eb8e90941a016113ebe8861
mikhailkravets <michkravets@gmail.com>
2025-01-13 16:21:40 +0200

    * Remove too complex task on functions * Add the first task on loops

---
+++ exercises/l4_loops/task1.py
@@ -0,0 +1,15 @@
+
+# Write the function body to make the script work without errors.
+# 
+# HINT:
+# Use while loop to count sum from 0 to n (included)
+def sum_to(n: int) -> int:
+    pass
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert sum_to(1) == 1
+    assert sum_to(2) == 3
+    assert sum_to(3) == 6
+    assert sum_to(4) == 10

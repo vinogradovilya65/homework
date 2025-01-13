90344b71c5e0561db62dbd0214a00f3fbcf36d21
mikhailkravets <michkravets@gmail.com>
2025-01-13 16:41:53 +0200

    Add 8 task on loops

---
+++ exercises/l4_loops/task8.py
@@ -0,0 +1,11 @@
+# Use `for` loop to calculate the amount
+# of positive number in a list of integers `n`
+def count_positive(n: list[int]) -> int:
+    pass
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert count_positive([1, 2, -3, -4]) == 2
+    assert count_positive([-1, -2, -3]) == 0
+    assert count_positive([4, 5, 4, 3]) == 4

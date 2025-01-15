bc504acf92de4e1b4118a8d9e2e66b33e5baaa34
mikekravets <michkravets@gmail.com>
2025-01-15 10:02:55 +0200

    Add tasks on lists

---
+++ exercises/l5_lists/task1.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    l = [1, 4, 3, 5, -1, 5]
+
+    # Modify the value of variable `c` using
+    # list `l` to make the script work without errors
+    c = None
+
+    # Do not change the below's code
+    assert c == 6
---
+++ exercises/l5_lists/task3.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+
+# Write the body of the function that returns
+# the first element from the list `l`
+def first(l: list[Any]) -> Any:
+    pass
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert first([1, 5, 4]) == 1
+    assert first(["a", "b", "c"]) == "a"
+    assert first([True, False]) == True

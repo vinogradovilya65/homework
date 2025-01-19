a79f05355e163fecdf99bf4e9f6b3f6bb3266c15
mikekravets <michkravets@gmail.com>
2025-01-19 13:03:35 +0200

    Add tasks on dicts and sets

---
+++ exercises/l7_dict_and_set/task8.py
@@ -0,0 +1,17 @@
+# Write a body of the function that counts
+# a character occurrences in a string and
+# returns a dictionary where key is a character
+# and value is the amount of this character in a string `s`
+#
+# For example,
+# 1. "aabc" -> {"a": 2, "b": 1, "c": 1}
+# 2. "" -> {}
+def count_chars(s: str) -> dict[str, int]:
+    pass
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert count_chars("aabc") == {"a": 2, "b": 1, "c": 1}
+    assert count_chars("abc") == {"a": 1, "b": 1, "c": 1}
+    assert count_chars("") == {}

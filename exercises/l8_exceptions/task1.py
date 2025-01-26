71a1015c201d11229293837749e493cb23846d7d
mikhailkravets <michkravets@gmail.com>
2025-01-26 17:45:33 +0200

    Add exceptions tasks

---
+++ exercises/l8_exceptions/task1.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    l = [1, 2, 0, 3]
+
+    s = 0
+    for i, v in enumerate(l):
+        # Use try...except block to handle the error.
+        # If error happens add nothing to `s`, just continue the loop
+        s = i / v
---
+++ exercises/l8_exceptions/task2.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+def peekaboo(l: list[str]) -> str:
+    i = randint(0, 30)
+
+    # Use try...except block to handle the exception.
+    # If success return the below's line of code, otherwise `return "boo"`
+    return f"peek {l[i]}"
+
+
+if __name__ == "__main__":
+    l = ["a", "b", "c", "ubu", "baba", "daba", "ububu"]
+    for i in range(15):
+        print(peekaboo(l))

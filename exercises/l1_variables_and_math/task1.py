dd25ff308ae12a63d0a333ec5ac93726e8b27263
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 19:51:12 +0200

    Update task1.py

--- exercises/l1_variables_and_math/task1.py
+++ exercises/l1_variables_and_math/task1.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     # Modify variable `c` to make this script work without errors.
     # Use variables a and b to build a value for variable c
-    c = None
+    c = a + b
 
     # Do not change the line below
     assert c == 15

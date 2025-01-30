550b78b4a7770cbe83eb41c273f3de0b68be459f
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:07:26 +0200

    Update task3.py

--- exercises/l1_variables_and_math/task3.py
+++ exercises/l1_variables_and_math/task3.py
@@ -1,9 +1,10 @@
 if __name__ == "__main__":
     # Do not change the line below
-    a, b = input("Enter a: "), input("Enter b: ")
+    a, b = int(input("Enter a: ")), int(input("Enter b: "))
 
     # Modify variables a and b to make the script work without errors
 
     # Do not change the line below
     assert a * b
+
     

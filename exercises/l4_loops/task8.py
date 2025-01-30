32a533439f67130d233b4d964e4075a37a261b0b
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:45:53 +0200

    Update task8.py

--- exercises/l4_loops/task8.py
+++ exercises/l4_loops/task8.py
@@ -1,7 +1,9 @@
-# Use `for` loop to calculate the amount
-# of positive number in a list of integers `n`
 def count_positive(n: list[int]) -> int:
-    pass
+    count = 0  # Инициализируем счётчик положительных чисел
+    for num in n:  # Проходим по всем элементам списка
+        if num > 0:  # Если число положительное
+            count += 1  # Увеличиваем счётчик
+    return count  # Возвращаем количество положительных чисел
 
 
 # Do not change the below's code

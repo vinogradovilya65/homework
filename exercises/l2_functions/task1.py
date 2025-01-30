867e5e8edec152a8d55fac404cb38c7fc0b45524
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:17:07 +0200

    Update task1.py

--- exercises/l2_functions/task1.py
+++ exercises/l2_functions/task1.py
@@ -1,11 +1,9 @@
 Number = int | float | complex
 
-
 # Write the body of the function to make the script
 # work without errors
 def sqr(n: Number) -> Number:
-    pass
-
+    return n ** 2  # Возводим число в квадрат
 
 if __name__ == "__main__":
     # Do not change the below asserts

fa198e92fa1afcf978657d9648bbd88fb14f2975
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 21:08:30 +0200

    Update task8.py

--- exercises/l7_dict_and_set/task8.py
+++ exercises/l7_dict_and_set/task8.py
@@ -1,14 +1,11 @@
-# Write a body of the function that counts
-# a character occurrences in a string and
-# returns a dictionary where key is a character
-# and value is the amount of this character in a string `s`
-#
-# For example,
-# 1. "aabc" -> {"a": 2, "b": 1, "c": 1}
-# 2. "" -> {}
 def count_chars(s: str) -> dict[str, int]:
-    pass
-
+    char_count = {}
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 # Do not change the below's code
 if __name__ == "__main__":

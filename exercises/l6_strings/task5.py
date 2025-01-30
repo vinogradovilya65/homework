eea66fef92768a98e2c9805fdb8afa046c8bdb15
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 21:02:55 +0200

    Update task5.py

--- exercises/l6_strings/task5.py
+++ exercises/l6_strings/task5.py
@@ -1,12 +1,5 @@
-# Write a function that returns True if string `s`
-# has a prefix `prefix` and False otherwise.
-#
-# For example,
-# 1. has_prefix("apple", "app") -> True
-# 2. has_prefix("apple", "applg") -> False
 def has_prefix(s: str, prefix: str) -> bool:
-    pass
-
+    return s.startswith(prefix)
 
 # Do not change the below's code
 if __name__ == "__main__":

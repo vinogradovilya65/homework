cc269411cfdfca08bd153249f381c513c0e0ae51
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:21:05 +0200

    Update task4.py

--- exercises/l2_functions/task4.py
+++ exercises/l2_functions/task4.py
@@ -1,7 +1,14 @@
+from collections.abc import Iterable
+
 # Declare and write the body of the function named `my_len`.
 # This function should accept an Iterable as a parameter
 # and return its length
 
+def my_len(iterable: Iterable) -> int:
+    count = 0
+    for _ in iterable:  # Проходим по каждому элементу в итерируемом объекте
+        count += 1       # Увеличиваем счетчик на 1
+    return count
 
 # Do not change the below's code
 if __name__ == "__main__":

377c5f66b1717da3fa25270fa9c5648548e15a85
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:22:23 +0200

    Update task7.py

--- exercises/l2_functions/task7.py
+++ exercises/l2_functions/task7.py
@@ -1,6 +1,9 @@
 # Declare the function named `greet` and write its body
 # the way that it fixes all errors in the script
 
+def greet(name: str) -> str:
+    return f"Hello, {name}!"  # Формируем строку с приветствием
+
 # Do not change the below's code
 if __name__ == "__main__":
     assert greet("Jon") == "Hello, Jon!"

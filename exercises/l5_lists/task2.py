6bb28061b6518d6e904b20ebbe726f7fb0b7eee0
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:46:57 +0200

    Update task2.py

--- exercises/l5_lists/task2.py
+++ exercises/l5_lists/task2.py
@@ -1,10 +1,9 @@
-
 if __name__ == "__main__":
     l = [1, 5, 4]
 
     # Modify the value of variable `c` using
     # list `l` to make the script work without errors
-    c = None
+    c = sum(l)  # Суммируем все элементы списка
 
     # Do not change the below's code
     assert c == 10

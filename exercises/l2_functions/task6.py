ebd28d462642f80a37d6e79fd0ddd41611e6133a
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:22:01 +0200

    Update task6.py

--- exercises/l2_functions/task6.py
+++ exercises/l2_functions/task6.py
@@ -1,10 +1,9 @@
-
 # Modify this function to make the script work without errors.
-def multiply(a: int, b: int) -> int:
+def multiply(a: int, b: int = 0) -> int:
     return a * b
-
 
 # Do not change the below's code
 if __name__ == "__main__":
     assert multiply(2, 2) == 4
     assert multiply(2) == 0
+

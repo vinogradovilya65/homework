5c84e9143c6ab04004c66dc2bb4b31b33248fe70
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:08:36 +0200

    Update task4.py

--- exercises/l1_variables_and_math/task4.py
+++ exercises/l1_variables_and_math/task4.py
@@ -1,6 +1,9 @@
 if __name__ == "__main__":
     # Do not change the line below
-    a = b = c = d = None
+    a = 10            # int
+    b = 10.5          # float
+    c = 3 + 4j        # complex
+    d = "Hello"       # str
 
     # Assign the values of correct types to variables a, b, c, d 
     # to make the script work without errors

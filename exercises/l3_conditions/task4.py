e678f0335354876fda0c1097a6fcf32d32e80fa9
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:25:25 +0200

    Update task4.py

--- exercises/l3_conditions/task4.py
+++ exercises/l3_conditions/task4.py
@@ -1,7 +1,6 @@
 # Write body of the function to make the script work without errors
 def can_vote(age: int) -> bool:
-    pass
-
+    return age >= 18  # Человек может голосовать, если его возраст 18 или больше
 
 # Do not change the below's code
 if __name__ == "__main__":
@@ -9,3 +8,4 @@ if __name__ == "__main__":
     assert can_vote(18) is True
     assert can_vote(118) is True
     assert can_vote(1100) is True
+

bd9ccbc68ef989143906263b9259d3db8955c534
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:44:22 +0200

    Update task4.py

--- exercises/l4_loops/task4.py
+++ exercises/l4_loops/task4.py
@@ -1,9 +1,13 @@
-# Use `for` loop to find the max value in the array (list)
-# of integers `l`.
-#
-# Return `None`, if list is empty
 def find_max(l: list[int]) -> int | None:
-    pass
+    if not l:  # Проверяем, пуст ли список
+        return None
+    
+    max_val = l[0]  # Начинаем с первого элемента
+    for num in l:  # Перебираем все элементы списка
+        if num > max_val:  # Если текущий элемент больше max_val, обновляем max_val
+            max_val = num
+    
+    return max_val  # Возвращаем максимальное значение
 
 
 # Do not change the below's code
@@ -12,3 +16,4 @@ if __name__ == "__main__":
     assert find_max([3, 1, 4, 3, 8, 7]) == 8
     assert find_max([1]) == 1
     assert find_max([]) == None
+

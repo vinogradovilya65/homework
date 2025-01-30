84e7f0193d72f318ffa3baf52b71fb08ed7256be
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:42:11 +0200

    Update task1.py

--- exercises/l4_loops/task1.py
+++ exercises/l4_loops/task1.py
@@ -1,14 +1,20 @@
-# Write the function body to make the script work without errors.
-#
-# HINT:
-# Use while loop to count sum from 0 to n (included)
-def sum_to(n: int) -> int:
-    pass
+# Write the body of the function to make the script work without errors
+def grade(score: int) -> str:
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"  # Для всех баллов ниже 60 возвращаем "F"
 
-
-# Do not change the below's code
 if __name__ == "__main__":
-    assert sum_to(1) == 1
-    assert sum_to(2) == 3
-    assert sum_to(3) == 6
-    assert sum_to(4) == 10
+    # Do not change the below asserts
+    assert grade(95) == "A"
+    assert grade(85) == "B"
+    assert grade(75) == "C"
+    assert grade(65) == "D"
+    assert grade(50) == "F"

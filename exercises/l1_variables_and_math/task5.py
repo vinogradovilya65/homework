32fe82de418f2715b5ed5d8fdbce9b974d711083
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 20:09:13 +0200

    Update task5.py

--- exercises/l1_variables_and_math/task5.py
+++ exercises/l1_variables_and_math/task5.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     # Do not change 2 lines below
-    a = 3.23
+    a = 3.72  # Теперь a равно b
     b = 3.72
 
     # Modify variables a, b to make the script work without errors
